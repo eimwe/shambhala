f978ed4ab78c07def8548e009177308f85898479
eimwe <prokopenkoserge@inbox.ru>
2023-01-31 19:41:36 +0600

    Instantiated Swiper on Schedule gallery

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,16 @@
-console.log('hello, world');
+import Swiper from './swiper-bundle.esm.browser.js';
+
+const ScheduleGallery = new Swiper('.schedule__gallery', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
