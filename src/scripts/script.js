91e252d9b83b5bb9b9d16fd9863d1406f144c8bc
eimwe <prokopenkoserge@inbox.ru>
2023-02-02 17:33:51 +0600

    Instantiated Swiper on Team gallery

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -14,3 +14,20 @@ const ScheduleGallery = new Swiper('.schedule__gallery', {
   mousewheel: true,
   keyboard: true,
 });
+
+const TeamGallery = new Swiper('.team__gallery', {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
