af7e314a7d425a2acbbbe9f4615c03ba792e8bbe
eimwe <prokopenkoserge@inbox.ru>
2023-02-06 19:28:13 +0600

    Saved confirm modal node and related buttons

---
+++ src/scripts/modals.js
@@ -0,0 +1,8 @@
+const confirmModal = document.querySelector('.modal--confirm');
+let confirmModalButtons = document.querySelectorAll('.modal--confirm .btn');
+
+confirmModalButtons.forEach(button => {
+  button.addEventListener('click', () => confirmModal.close());
+});
+
+export { confirmModal };
