55442d8bc0aebf1e7c109c6fc1aa2252c0afb94e
eimwe <prokopenkoserge@inbox.ru>
2023-02-07 13:22:15 +0600

    Added menu modal hide/show logic

--- src/scripts/modals.js
+++ src/scripts/modals.js
@@ -5,4 +5,15 @@ confirmModalButtons.forEach(button => {
   button.addEventListener('click', () => confirmModal.close());
 });
 
+const menuModal = document.querySelector('.modal--menu');
+const menuModalOpenBtn = document.querySelector('.btn--menu');
+const menuModalCloseBtn = document.querySelector('.modal--menu .btn--dismiss');
+const menuModalNavItems = document.querySelectorAll('.modal--menu .navbar__item');
+
+menuModalOpenBtn.addEventListener('click', () => menuModal.showModal());
+menuModalCloseBtn.addEventListener('click', () => menuModal.close());
+menuModalNavItems.forEach(item => {
+  item.addEventListener('click', () => menuModal.close());
+});
+
 export { confirmModal };
