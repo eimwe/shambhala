52778924a10f0699c5bb4dedb1843bbabf1bd0a6
eimwe <prokopenkoserge@inbox.ru>
2023-02-13 19:17:17 +0600

    Added breakpoint for large viewports

--- src/scripts/breakpoints.js
+++ src/scripts/breakpoints.js
@@ -1,4 +1,5 @@
 let small = window.matchMedia('(min-width: 0px) and (max-width: 601px)'),
-    medium = window.matchMedia('(min-width: 0px) and (max-width: 901px)');
+    medium = window.matchMedia('(min-width: 0px) and (max-width: 901px)'),
+    large = window.matchMedia('(min-width: 900px)');
 
-export {small, medium};
+export {small, medium, large};
