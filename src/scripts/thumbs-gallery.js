be406505140819890305c6fbed99a109b10b623a
eimwe <prokopenkoserge@inbox.ru>
2023-02-15 14:07:06 +0600

    Added Swiper thumb gallery for thumbs modal
    Depended its functioning on media query breakpoint

---
+++ src/scripts/thumbs-gallery.js
@@ -0,0 +1,37 @@
+import Swiper from './swiper-bundle.esm.browser.js';
+import { large } from './breakpoints.js';
+
+let FullsizeGallery, ThumbsGallery, initThumbs = false;
+
+const initThumbsOnDesktop = () => {
+  if (large.matches) {
+    if (!initThumbs) {
+      initThumbs = true;
+      ThumbsGallery = new Swiper('.gallery__bottom', {
+        slidesPerView: 'auto',
+        loop: true,
+        freeMode: true,
+        loopedSlides: 14, //looped slides should be the same
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      FullsizeGallery = new Swiper('.gallery__top', {
+        loop: true,
+        loopedSlides: 14, //looped slides should be the same
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+          swiper: ThumbsGallery,
+        },
+      });
+    }
+  } else if (initThumbs) {
+    FullsizeGallery.destroy();
+    ThumbsGallery.destroy();
+    initThumbs = false;
+  }
+}
+
+export default initThumbsOnDesktop;
