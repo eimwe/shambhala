050cab7247b317c192c20a04c8e34903a43cf4c0
eimwe <prokopenkoserge@inbox.ru>
2023-02-19 19:38:59 +0600

    Created 'functions' folder
    Moved year setter and phone mask functions into this folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,4 @@ import * as galleries from './components/galleries/index.js';
 import * as modals from './components/modals/index.js';
 import * as accordion from './components/accordion/index.js';
 import * as form from './components/form/index.js';
-import PhoneMask from './phone-mask.js';
-import setCurrentyear from './year-setter.js';
-
-PhoneMask('.form__control--phone');
-
-setCurrentyear('.copyright__year');
+import * as functions from './functions/index.js';
